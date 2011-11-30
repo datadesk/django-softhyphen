faec7214c9590f7661514849dc625f1f70d0a669
palewire <ben.welsh@gmail.com>
2011-11-29 21:17:30 -0800

    Added a blacklist test

--- softhyphen/tests.py
+++ softhyphen/tests.py
@@ -4,15 +4,20 @@ from django.test import TestCase
 
 class SoftHyphenTest(TestCase):
     
-    def test_method(self):
+    def test_simple_call(self):
         """
-        Test usage of the hyphenation method directly.
+        Test simple usage of the hyphenation method directly.
         """
         before = "<h1>I love hyphenation</h1>"
         after = hyphenate_html(before)
         self.failUnlessEqual(after, "<h1>I love hy&shy;phen&shy;a&shy;tion</h1>")
-
-
-
+    
+    def test_tag_blacklist(self):
+        """
+        Test the blacklist to make sure some tags don't get hyphenated.
+        """
+        before = "<code>I love hyphenation</code>"
+        after = hyphenate_html(before)
+        self.failUnlessEqual(after, "<code>I love hyphenation</code>")
 
 
