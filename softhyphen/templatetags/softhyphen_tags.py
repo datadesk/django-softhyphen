010e65fe1aa4d8ddd73287bb46569ce8f2b6c32b
Lenz Hirsch <hirsch@seamless.de>
2012-06-06 17:09:57 +0200

    get active language from django.utils.translation by default

--- softhyphen/templatetags/softhyphen_tags.py
+++ softhyphen/templatetags/softhyphen_tags.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from django import template
-register = template.Library()
-from softhyphen.html import hyphenate
 from django.utils.safestring import mark_safe
 from django.template.defaultfilters import stringfilter
-#from django.conf import Settings  #TODO: get default language from settings
+from django.utils import translation
 from django.core.cache import cache
+
+from softhyphen.html import hyphenate
 import hashlib
-from time import time
+
+register = template.Library()
 
 
-def softhyphen(value, language='en-US'):
+def softhyphen(value, language=translation.get_language()):
     """
     Hyphenates html.
     """
@@ -21,23 +22,18 @@ softhyphen = stringfilter(softhyphen)
 softhyphen = register.filter(softhyphen)
 
 
-def cached_softhyphen(value, language='de-DE'):
+def cached_softhyphen(value, language=translation.get_language()):
     """
     Hyphenates html and caches result.
     """
-    start = time()
+    print translation.get_language()
     md5 = hashlib.md5()
     md5.update(value.encode('utf-8'))
     digest = md5.hexdigest()
     hyph_html = cache.get("softhypen_cache_" + digest)
-    end = time()
-    print "hit: ", end - start
     if not hyph_html:
-        start = time()
         hyph_html = hyphenate(value, language=language)
         cache.set("softhypen_cache_" + digest, 0)
-        end = time()
-        print "no hit: ", end - start
     return mark_safe(hyph_html)
 
 cached_softhyphen.is_safe = True
