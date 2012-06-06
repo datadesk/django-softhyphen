980f2455ff5074c11b8f06663f2908424ce7777b
Lenz Hirsch <hirsch@seamless.de>
2012-06-06 16:04:40 +0200

    added a template tag "cached_softhyphe"n which uses a md5 hexdigest over the value to cache the hyphenated version of value

--- softhyphen/templatetags/softhyphen_tags.py
+++ softhyphen/templatetags/softhyphen_tags.py
@@ -4,15 +4,42 @@ register = template.Library()
 from softhyphen.html import hyphenate
 from django.utils.safestring import mark_safe
 from django.template.defaultfilters import stringfilter
+#from django.conf import Settings  #TODO: get default language from settings
+from django.core.cache import cache
+import hashlib
+from time import time
 
 
-def softhyphen(value, language="en-us"):
+def softhyphen(value, language='en-US'):
     """
     Hyphenates html.
     """
     return mark_safe(hyphenate(value, language=language))
+
 softhyphen.is_safe = True
 softhyphen = stringfilter(softhyphen)
 softhyphen = register.filter(softhyphen)
 
 
+def cached_softhyphen(value, language='de-DE'):
+    """
+    Hyphenates html and caches result.
+    """
+    start = time()
+    md5 = hashlib.md5()
+    md5.update(value.encode('utf-8'))
+    digest = md5.hexdigest()
+    hyph_html = cache.get("softhypen_cache_" + digest)
+    end = time()
+    print "hit: ", end - start
+    if not hyph_html:
+        start = time()
+        hyph_html = hyphenate(value, language=language)
+        cache.set("softhypen_cache_" + digest, 0)
+        end = time()
+        print "no hit: ", end - start
+    return mark_safe(hyph_html)
+
+cached_softhyphen.is_safe = True
+cached_softhyphen = stringfilter(cached_softhyphen)
+cached_softhyphen = register.filter(cached_softhyphen)
