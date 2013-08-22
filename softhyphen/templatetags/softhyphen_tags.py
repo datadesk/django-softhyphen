7a858c87812619ff44dfea3a2bb6be2569902d17
Brent O'Connor <epicserve@gmail.com>
2013-08-22 12:55:20 -0700

    Removed DeprecationWarning

--- softhyphen/templatetags/softhyphen_tags.py
+++ softhyphen/templatetags/softhyphen_tags.py
@@ -3,16 +3,11 @@ from django import template
 register = template.Library()
 from softhyphen.html import hyphenate
 from django.utils.safestring import mark_safe
-from django.template.defaultfilters import stringfilter
 
 
+@register.filter(is_safe=True)
 def softhyphen(value, language="en-us"):
     """
     Hyphenates html.
     """
     return mark_safe(hyphenate(value, language=language))
-softhyphen.is_safe = True
-softhyphen = stringfilter(softhyphen)
-softhyphen = register.filter(softhyphen)
-
-
