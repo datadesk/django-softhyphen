f946436c156839808a30f19a99ca1e6f47185bee
Brent O'Connor <epicserve@gmail.com>
2013-08-22 16:58:54 -0700

    Fixed the filter so it works in older versions of Django

--- softhyphen/templatetags/softhyphen_tags.py
+++ softhyphen/templatetags/softhyphen_tags.py
@@ -2,12 +2,13 @@
 from django import template
 register = template.Library()
 from softhyphen.html import hyphenate
-from django.utils.safestring import mark_safe
 
 
-@register.filter(is_safe=True)
+@register.filter
 def softhyphen(value, language="en-us"):
     """
     Hyphenates html.
     """
-    return mark_safe(hyphenate(value, language=language))
+    return hyphenate(value, language=language)
+
+softhyphen.is_safe = True
