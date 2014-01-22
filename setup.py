a98dcd0f418d63f128fe201a90757311a92ca526
palewire <ben.welsh@gmail.com>
2014-01-21 22:54:34 -0800

    Upgraded version in setup.py

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
 
 setup(
       name='django-softhyphen',
-      version='0.2.0',
+      version='0.3.0',
       description='A Python library for hyphenating HTML in your Django project',
       author='Ben Welsh',
       author_email='ben.welsh@gmail.com',
