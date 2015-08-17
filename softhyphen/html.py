59a3cb82e70d07003cc01c9f4b92f8a59ba2d120
Jacob Rief <jacob.rief@gmail.com>
2015-08-17 20:20:14 +0200

    fixes PEP8 warning

--- softhyphen/html.py
+++ softhyphen/html.py
@@ -11,10 +11,6 @@ import six
 from bs4 import BeautifulSoup
 from .hyphenator import Hyphenator
 from bs4.element import PreformattedString
-from django.conf import settings
-
-BEAUTIFULSOUP_BUILDER_FEATURES = getattr(settings,
-    'SOFTHYPHEN_BEAUTIFULSOUP_BUILDER_FEATURES', ['html.parser'])
 
 
 class DontEscapeDammit(PreformattedString):
@@ -55,7 +51,7 @@ def hyphenate(html, language='en-us', hyphenator=None, blacklist_tags=(
         hyphenator = get_hyphenator_for_language(language)
 
     # Create HTML tree
-    soup = BeautifulSoup(html, features=BEAUTIFULSOUP_BUILDER_FEATURES)
+    soup = BeautifulSoup(html, "html.parser")
 
     # Recursively hyphenate each element
     hyphenate_element(soup, hyphenator, blacklist_tags)
